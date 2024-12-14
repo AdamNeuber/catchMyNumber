752380e50bcfa9fa1bcd79016e1771546fcb4dc5
NeuberA <adamneuber1@gmail.com>
2024-12-14 10:29:54 +0100

    Introduction added

--- src/catchmynumber.ts
+++ src/catchmynumber.ts
@@ -9,6 +9,10 @@ const myNumber: number = randint(1, 100);
 let a: number = 0
 let pokus: number = 1
 
+console.log("Welcome")
+console.log("I'm thinking of a number between 1 and 100.")
+console.log("You have to guess it.")
+
 while(a != myNumber) {
     a = Number(prompt("Enter a number"))
 
